2e61c619ac1c69e69e5d853997113e30c12d64b7
David Kielty <davidkielty@live.co.uk>
2024-01-29 12:35:21 +0000

    Auth base first

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs, resolvers } from './graphql';
 import { GqlContext } from './graphql/types';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const server = new ApolloServer<GqlContext>({ typeDefs, resolvers });
-
-const port = Number(process.env.PORT ?? 8080);
+// const port = Number(process.env.PORT ?? 8080);
 const startServer = async () => {
   const { url } = await startStandaloneServer(server, {
-    listen: { host: '0.0.0.0', port },
+    // listen: { host: '0.0.0.0', port },
+    listen: { host: '0.0.0.0', port: 8080 },
+
     context: async () => {
       return {};
     },
